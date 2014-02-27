674e6c969cfc857f5a52d692ef76668d2aa4129e
Trisha Gee <trishagee@github.com>
2014-02-27 20:31:04 +0100

    [Step 3] Add tea and order the drinks alphabetically

--- src/main/resources/html/js/coffee.js
+++ src/main/resources/html/js/coffee.js
@@ -4,6 +4,7 @@ coffeeApp.controller('DrinksController', function ($scope) {
     $scope.types = [
         {name: 'Americano', family: 'Coffee'},
         {name: 'Latte', family: 'Coffee'},
+        {name: 'Tea', family: 'That Other Drink'},
         {name: 'Cappuccino', family: 'Coffee'}
     ]
 })
